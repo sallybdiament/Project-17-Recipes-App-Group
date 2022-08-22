cb57138444a8ef1aebd758ef69d6b3f0028c46aa
Tales Rodrigues <tales00001@gmail.com>
2022-08-22 09:46:26 -0300

    Requisitos 8 e 9: finalizando Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title }) {
+  const [isSearchInputOpened, toggleSearchInput] = useState(false);
+
   return (
     <div>
-      <img
-        src={ profileIcon }
-        data-testid="profile-top-btn"
-        alt="Ícone de perfil"
-      />
+      <Link to="/profile">
+        <img
+          src={ profileIcon }
+          data-testid="profile-top-btn"
+          alt="Ícone de perfil"
+        />
+      </Link>
       {(title !== 'Profile'
-      && title !== 'Done Recipes'
-      && title !== 'Favorite Recipes')
-      && <img
-        src="../images/searchIcon.svg"
-        data-testid="search-top-btn"
-        alt="Ícone de pesquisa"
-      />}
+        && title !== 'Done Recipes'
+        && title !== 'Favorite Recipes')
+        && (
+          <button
+            type="button"
+            onClick={ () => toggleSearchInput(!isSearchInputOpened) }
+          >
+            <img
+              src={ searchIcon }
+              data-testid="search-top-btn"
+              alt="Ícone de pesquisa"
+            />
+          </button>
+        )}
+      {isSearchInputOpened && <input data-testid="search-input" />}
       <h1 data-testid="page-title">{title}</h1>
     </div>
   );
