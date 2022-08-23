07e7bf8497fad84ebbd56b6da34b9d2130155bb4
Renata Costa <rrenata_kelly@hotmail.com>
2022-08-23 16:53:08 -0300

    requisito 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const emailValido = /\S+@\S+\.\S+/;
+    const sete = 7;
+    if (emailValido.test(form.email) && form.password.length >= sete) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [form.email, form.password, form]);
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
     <div>
       <form>
@@ -12,6 +34,9 @@ function Login() {
             type="email"
             data-testid="email-input"
             id="email-input"
+            name="email"
+            value={ form.email }
+            onChange={ handleChange }
           />
         </label>
         <label
@@ -22,12 +47,15 @@ function Login() {
             type="password"
             data-testid="password-input"
             id="passoword-input"
+            name="password"
+            value={ form.password }
+            onChange={ handleChange }
           />
         </label>
         <button
           type="button"
           data-testid="login-submit-btn"
-          disabled="true"
+          disabled={ disabled }
         >
           Enter
         </button>
