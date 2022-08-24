40abbb90232a96b4bae9785cfc0cff091a974ebe
Sally Diament <sally.barzilai@gmail.com>
2022-08-24 20:10:09 -0300

    tentando o copy to clipboard

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Header from '../components/Header';
+// https://www.npmjs.com/package/react-hook-clipboard
+// import useClipboard from 'react-hook-clipboard'
 import shareIcon from '../images/shareIcon.svg';
 
 export default function DoneRecipes() {
-  const doneRecipesLocalStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+  // const doneRecipesLocalStorage = JSON.parse(localStorage.getItem('doneRecipes'));
   const numberThree = 3;
-  // const doneRecipesLocalStorage = [{
-  //   id: '52768',
-  //   // idMeal
-  //   type: 'food',
-  //   nationality: 'British',
-  //   // strArea
-  //   category: 'Dessert',
-  //   // strCategory
-  //   alcoholicOrNot: '',
-  //   // strDrinkAlternate
-  //   name: 'Apple Frangipan Tart',
-  //   // strMeal
-  //   image: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
-  //   // strMealThumb
-  //   doneDate: '2/2/22',
-  //   // "dateModified": null
-  //   tags: ['Tart', 'Baking', 'Fruity'],
-  //   // "strTags": "Tart,Baking,Fruity",
-  // }];
+  const doneRecipesLocalStorage = [{
+    id: '52768',
+    // idMeal
+    type: 'food',
+    nationality: 'British',
+    // strArea
+    category: 'Dessert',
+    // strCategory
+    alcoholicOrNot: '',
+    // strDrinkAlternate
+    name: 'Apple Frangipan Tart',
+    // strMeal
+    image: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
+    // strMealThumb
+    doneDate: '2/2/22',
+    // "dateModified": null
+    tags: ['Tart', 'Baking', 'Fruity'],
+    // "strTags": "Tart,Baking,Fruity",
+  }];
 
   return (
     <div>
@@ -52,35 +55,42 @@ export default function DoneRecipes() {
         if (recipe.type === 'meals') link = '/foods/';
         else link = '/drinks/';
         return (
-          <Link
-            // to={ `${link}${recipe[id]}` }
-            to={ link }
-            key={ recipe.id }
-            data-testid={ `${index}-recipe-card` }
-          >
-            <img
-              src={ shareIcon }
-              alt="Share Icon"
-              data-testid={ `${index}-horizontal-share-btn` }
-            />
+          <div key={ recipe.id }>
+            <button
+              type="button"
+              onClick={ () => copy('A') }
+            >
+              <img
+                src={ shareIcon }
+                alt="Share Icon"
+                data-testid={ `${index}-horizontal-share-btn` }
+              />
+            </button>
             <p
               data-testid={ `${index}-horizontal-top-text` }
             >
-              { `${recipe.nationality} - ${recipe.category}`}
+              { `${recipe.nationality} - ${recipe.category} - ${recipe.alcoholicOrNot}`}
 
             </p>
-            <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              alt={ recipe.name }
-              src={ recipe.image }
-            />
+            <Link
+              to={ `${link}${recipe.id}` }
+              // to={ link }
+              key={ recipe.id }
+              data-testid={ `${index}-recipe-card` }
+            >
+              <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                alt={ recipe.name }
+                src={ recipe.image }
+              />
+            </Link>
             <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
             { recipe.tags.map((tag, i) => (
               index < numberThree
               && <p key={ i } data-testid={ `${index}-${tag}-horizontal-tag` }>{tag}</p>
             ))}
-          </Link>
+          </div>
         );
       })}
     </div>
--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,10 +1,79 @@
 import React from "react";
 import { screen } from '@testing-library/react'
 import renderWithRouter from "./helpers/renderWithRouter";
-import userEvent from '@testing-library/user-event';
+import { render, fireEvent, waitForElement } from "@testing-library/react";
 import App from "../App";
 import DoneRecipes from "../pages/DoneRecipes";
 import AppProvider from "../context/AppProvider";
+
+ const data = [{
+    id: '52768',
+    // idMeal
+    type: 'food',
+    nationality: 'British',
+    // strArea
+    category: 'Dessert',
+    // strCategory
+    alcoholicOrNot: '',
+    // strDrinkAlternate
+    name: 'Apple Frangipan Tart',
+    // strMeal
+    image: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
+    // strMealThumb
+    doneDate: '2/2/22',
+    // "dateModified": null
+    tags: ['Tart', 'Baking', 'Fruity'],
+    // "strTags": "Tart,Baking,Fruity",
+  }];
+
+// https://javascript.plainenglish.io/testing-local-storage-with-testing-library-580f74e8805b
+describe("DoneRecipes tests", () => {
+  const fakeAxios = {
+    get: jest.fn(() => Promise.resolve({doneRecipes: data}))
+  };
+
+  beforeEach(() => {
+    Object.defineProperty(window, "localStorage", {
+      value: {
+        getItem: jest.fn(() => null),
+        setItem: jest.fn(() => null)
+      },
+      writable: true
+    });
+  });
+  
+  it("Should call localStorage getItem on render", () => {
+    const { history } =  renderWithRouter(
+      <AppProvider>
+      <App axios={fakeAxios} />
+      </AppProvider>);
+      history.push('/done-recipes');
+
+    expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+});
+
+https://thewebdev.info/2022/02/24/how-to-mock-local-storage-in-jest-tests/
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    }
+  };
+})();
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+});
 
 describe('Testes com o componente DoneRecipes', () => {
   it('Verifica se ao renderizar o componente DoneRecipes aparecem o título Done Recipes', () => {
@@ -16,13 +85,16 @@ describe('Testes com o componente DoneRecipes', () => {
 
     const doneRecipesEl = screen.getByRole('heading', {  name: /done recipes/i})
     expect(doneRecipesEl).toBeInTheDocument();
+
+    const buttonsEl = screen.getAllByRole('button');
+    expect(buttonsEl).toHaveLength(3);
   });
   
   it('Verifica se ao clicar no food-bottom-btn é redirecionado para página /foods', () => {
     const { history } =  renderWithRouter(
       <AppProvider>
       <App />
-      </AppProvider>);
+      </AppProvider>);q
       history.push('/done-recipes');
    
     const profileIconEl = screen.getByRole('img', {  name: /ícone de perfil/i});
