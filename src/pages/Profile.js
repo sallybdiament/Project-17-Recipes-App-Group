6d5e6a0022ec5ebc21a6f9c503e2c35414c7d93b
Renata Costa <rrenata_kelly@hotmail.com>
2022-08-25 17:50:47 -0300

    requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,6 +7,27 @@ export default function Profile() {
   return (
     <div>
       <Header title="Profile" />
+      <div>
+        <p data-testid="profile-email">email</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
