68b3a9f8bd1b8910c8dd51b0ad6928a3eead610f
Renata Costa <rrenata_kelly@hotmail.com>
2022-08-25 20:18:58 -0300

    requisitos 60 e 61

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -15,6 +15,16 @@ export default function Profile() {
     recuperaEmail();
   }, []);
 
+  const history = useHistory();
+
+  function btnOne() {
+    history.push('/done-recipes');
+  }
+
+  function btnTwo() {
+    history.push('/favorite-recipes');
+  }
+
   return (
     <div>
       <Header title="Profile" />
@@ -23,12 +33,14 @@ export default function Profile() {
         <button
           type="button"
           data-testid="profile-done-btn"
+          onClick={ btnOne }
         >
           Done Recipes
         </button>
         <button
           type="button"
           data-testid="profile-favorite-btn"
+          onClick={ btnTwo }
         >
           Favorite Recipes
         </button>
