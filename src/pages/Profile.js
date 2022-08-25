fe9c21e1161b47fd792ce2fbc1f11fa9d78b7391
Renata Costa <rrenata_kelly@hotmail.com>
2022-08-25 20:02:58 -0300

    requisitos 58 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const [email, setEmail] = useState('');
+
+  function recuperaEmail() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setEmail(user.email);
+  }
+
+  useEffect(() => {
+    recuperaEmail();
+  }, []);
+
   return (
     <div>
       <Header title="Profile" />
       <div>
-        <p data-testid="profile-email">email</p>
+        <p data-testid="profile-email">{email}</p>
         <button
           type="button"
           data-testid="profile-done-btn"
