b4cb05d74df4f0b1bae920ce896c900f489c9bed
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-08-30 10:16:23 -0300

    Corrigindo erro do header

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import Header from '../components/Header';
 import RecipesDetails from '../components/RecipeDetails';
 
 export default function Drinks() {
   return (
     <div>
-      <Header title="Details" />
       <RecipesDetails />
     </div>
   );
